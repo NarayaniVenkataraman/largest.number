8f0f48524fc2499cb291633f60a5776544a5d9e1
NarayaniVenkataraman <157119315+NarayaniVenkataraman@users.noreply.github.com>
2024-01-19 16:13:09 +0000

    Add files via upload

--- largest_number.py
+++ largest_number.py
@@ -1,7 +1,11 @@
-num1 = int(input ("Enter first number: "))
-num2 = int(input ("Enter second number: "))
-num3 = int(input ("Enter third number: "))
-   
+while True:
+    try:
+        num1 = int(input ("Enter first number: "))
+        num2 = int(input ("Enter second number: "))
+        num3 = int(input ("Enter third number: "))
+        break
+    except Exception:
+        print("Invalid input. Please enter an integer.")
 
 if num1>num2 and num1>num3:
     g1 = num1
